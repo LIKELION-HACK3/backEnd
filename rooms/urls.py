89c23aaa9bf873b6edda3fbec53ec74fff36da9a
hyunhan-cho <hyeonhanjo1@gmail.com>
2025-08-10 20:35:52 +0900

    feat: 피그마 디자인에 맞는 방 검색 기능 구현

--- rooms/urls.py
+++ rooms/urls.py
@@ -4,10 +4,14 @@ from .views import (
     RoomDetailView,
     ImportRoomsView,
     ReviewListCreateView,
+    RoomSearchView,
+    RoomStatsView,
 )
 
 urlpatterns = [
     path('', RoomListCreateView.as_view(), name='room-list'),
+    path('search/', RoomSearchView.as_view(), name='room-search'),
+    path('stats/', RoomStatsView.as_view(), name='room-stats'),
     path('<int:pk>/', RoomDetailView.as_view(), name='room-detail'),
     path('import/', ImportRoomsView.as_view(), name='room-import'),
     path('<int:room_id>/reviews/', ReviewListCreateView.as_view(), name='review-list-create'),
