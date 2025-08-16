6a7a72e4b5e812fa8c54d633f43e46ed876c36e7
hyunhan-cho <hyeonhanjo1@gmail.com>
2025-08-16 19:32:58 +0900

    bookmark swagger추가

--- bookmarks/views.py
+++ bookmarks/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import get_object_or_404
+from drf_spectacular.utils import extend_schema, OpenApiParameter, OpenApiResponse, OpenApiExample
 from rest_framework import generics, status
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
@@ -14,6 +15,17 @@ class BookmarkPagination(PageNumberPagination):
   page_size_query_param="page_size"
   max_page_size=10
 
+@extend_schema(
+  tags=['bookmarks'],
+  summary='북마크 목록 조회',
+  description='로그인한 사용자의 북마크 목록을 페이지네이션하여 반환합니다.',
+  responses={
+    200: OpenApiResponse(
+      response=BookmarkSerializer(many=True),
+      description='북마크 목록',
+    )
+  }
+)
 class BookmarkListView(generics.ListAPIView):
   permission_classes=[IsAuthenticated]
   serializer_class=BookmarkSerializer
@@ -28,6 +40,25 @@ class BookmarkListView(generics.ListAPIView):
       .order_by("-created_at", "-id")
     )
 
+@extend_schema(
+  tags=['bookmarks'],
+  summary='북마크 토글',
+  description='지정한 방을 북마크에 추가하거나 제거합니다.',
+  parameters=[
+    OpenApiParameter(name='room_id', description='방 ID', required=True, type=int),
+  ],
+  responses={
+    201: OpenApiResponse(
+      description='북마크 추가됨',
+      examples=[OpenApiExample('created', value={'bookmarked': True, 'room_id': 1}, response_only=True, status_codes=['201'])]
+    ),
+    200: OpenApiResponse(
+      description='북마크 해제됨',
+      examples=[OpenApiExample('deleted', value={'bookmarked': False, 'room_id': 1}, response_only=True, status_codes=['200'])]
+    ),
+    404: OpenApiResponse(description='방을 찾을 수 없습니다'),
+  }
+)
 class BookmarkToggleView(APIView):
   permission_classes=[IsAuthenticated]
 
